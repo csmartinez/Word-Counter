57473e1cd6e7a60821ffd0e8380338367044816e
Carli Martinez <martinezcarli@gmail.com>
2015-02-05 14:42:48 -0800

    Delete times_you_said_this_spec.rb

--- spec/times_you_said_this_spec.rb
+++
@@ -1,8 +0,0 @@
-require ('rspec')
-require ('times_you_said_this')
-
-describe('String#said') do
-  it ("splits string up into an array, deltes punctiation and counts second input") do
-    expect("who let the dogs out!".said()).to(eq(1))
-  end
-end
